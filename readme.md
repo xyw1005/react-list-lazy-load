6efe4ac16cc6ca1231731071958ef5b22c6f7b1d
René Kooi <rene@kooi.me>
2016-03-02 13:21:24 +0100

    init

---
+++ readme.md
@@ -0,0 +1,75 @@
+# react-list-lazy-load
+
+Lazy Loading <abbr title="Higher Order Component">HOC</abbr> Wrapper for
+[React-List](https://github.com/orgsync/react-list).
+
+## Installation
+
+```bash
+npm install --save react-list-lazy-load
+```
+
+## Usage
+
+React List Lazy Load is a higher order component, so you wrap it around a
+`<ReactList />` element to :sparkles: magically :sparkles: add lazy loading
+hooks.
+
+```js
+import ReactList from 'react-list';
+import LazyLoading from 'react-list-lazy-load';
+
+const MyList = ({ items, onRequestPage }) => (
+  <LazyLoading
+    length={items.length}
+    items={items}
+    onRequestPage={onRequestPage}
+  >
+    <ReactList
+      itemRenderer={(idx, key) => (
+        <div key={key}>{items[idx]}</div>
+      )}
+      type="uniform"
+      length={items.length}
+    />
+  </LazyLoading>
+);
+```
+
+## API
+
+### `items={Array}`
+
+An array of the items you're showing. This is used to determine when to load a
+page. If the user scrolls close to a `null` item in this array, or outside of
+array bounds, a new page will be loaded.
+
+I.e., a `null` item is regarded as an unloaded item.
+
+### `length={Number}`
+
+The total amount of items, on all "pages". Defaults to
+
+### `pageSize={Number}`
+
+Amount of items on a page. This is used to determine which page to load.
+Defaults to 25 (rather arbitrarily).
+
+### `loadMargin={Number}`
+
+When to start loading the next page. The next page will be loaded when the user
+scrolls within `loadMargin` items from an unloaded item. You'll want to change
+this depending on the size of your items. If your items are super small, you
+should pick a larger `loadMargin`, but if they are rather large, you might be
+good with a margin of like 1 or 2 items.
+
+Defaults to 5.
+
+### `onRequestPage={function(page, cb)}`
+
+Callback to load a page. This only tells you to load a new page--you should
+merge it into the `items` prop yourself.
+
+## Licence
+
+[MIT](./LICENSE)
---
+++ src/LazyList.js
@@ -0,0 +1,173 @@
+import { cloneElement, Component, PropTypes } from 'react'
+
+const proxyMethods = [
+  'getOffset',
+  'getScrollParent',
+  'getScroll',
+  'setScroll',
+  'getViewportSize',
+  'getScrollSize',
+  'getStartAndEnd',
+  'getItemSizeAndItemsPerRow',
+  'getSpaceBefore',
+  'getSizeOf',
+  'scrollTo',
+  'scrollAround',
+  'getVisibleRange'
+]
+
+function requestPage (call, page, cb) {
+  let promise = call(page, cb)
+  if (promise && promise.then) {
+    promise
+      .then((res) => cb(null, res))
+      .then(null, cb)
+  }
+}
+
+/**
+ * Like `.slice`, but doesn't care about array bounds.
+ *
+ * [0, 1].slice(1, 3) === [1]
+ * eagerSlice([0, 1], 1, 3) === [1, undefined, undefined]
+ */
+function eagerSlice (list, start, end) {
+  const sliced = []
+  for (let i = start; i < end; i++) {
+    sliced.push(list[i])
+  }
+  return sliced
+}
+
+/**
+ * Adds simple lazy loading to react-list.
+ */
+export default class LazyList extends Component {
+  static propTypes = {
+    /**
+     * Total amount of items, on all pages.
+     */
+    length: PropTypes.number.isRequired,
+    /**
+     * Items per page.
+     */
+    pageSize: PropTypes.number,
+    /**
+     * When to begin loading the next page.
+     */
+    loadMargin: PropTypes.number,
+    /**
+     * Loaded items. NULLs in this array indicate unloaded items.
+     */
+    items: PropTypes.array,
+
+    /**
+     * Callback to begin loading a page.
+     */
+    onRequestPage: PropTypes.func.isRequired
+  }
+
+  static defaultProps = {
+    pageSize: 25,
+    loadMargin: 5
+  }
+
+  constructor (props) {
+    super(props)
+
+    this._list = null
+    this._loadingPages = {}
+    this.updateFrame = this.updateFrame.bind(this)
+  }
+
+  componentDidMount () {
+    this.updateScrollParent()
+    this.updateFrame()
+  }
+
+  componentDidUpdate () {
+    this.updateScrollParent()
+    this.updateFrame()
+  }
+
+  updateScrollParent () {
+    const prev = this.scrollParent
+    this.scrollParent = this.getScrollParent()
+    if (prev === this.scrollParent) {
+      return
+    }
+    if (prev) {
+      prev.removeEventListener('scroll', this.updateFrame)
+    }
+    if (this.props.onRequestPage) {
+      this.scrollParent.addEventListener('scroll', this.updateFrame)
+    }
+  }
+
+  getList () {
+    return this._list
+  }
+
+  isLoadingPage (page) {
+    return !!this._loadingPages[page]
+  }
+
+  itemNeedsLoad (idx) {
+    const { items, pageSize } = this.props
+    const page = Math.floor(idx / pageSize)
+    return items[idx] != null || this.isLoadingPage(page)
+  }
+
+  updateFrame () {
+    const {
+      pageSize, loadMargin,
+      items, length,
+      onRequestPage
+    } = this.props
+
+    // Item range that should be loaded right about now.
+    let [topItem, bottomItem] = this.getVisibleRange()
+
+    if (topItem === undefined || bottomItem === undefined) {
+      return
+    }
+
+    topItem = Math.max(topItem - loadMargin, 0)
+    bottomItem = Math.min(bottomItem + loadMargin, length - 1)
+
+    const almostVisibleItems = eagerSlice(items, topItem, bottomItem)
+
+    const unloadedPages = almostVisibleItems.reduce((pages, item, idx) => {
+      if (item == null) {
+        const page = Math.floor((topItem + idx) / pageSize)
+        if (!this.isLoadingPage(page) && pages.indexOf(page) === -1) {
+          return [ ...pages, page ]
+        }
+      }
+      return pages
+    }, [])
+
+    unloadedPages.forEach((page) => {
+      this._loadingPages[page] = true
+      requestPage(onRequestPage, page, () => {
+        // Always delete after completion. If there was an error, we can retry
+        // later. If there wasn't, we don't need to keep this around :)
+        delete this._loadingPages[page]
+      })
+    })
+  }
+
+  render () {
+    return cloneElement(this.props.children, {
+      ref: (list) => {
+        this._list = list
+      }
+    })
+  }
+}
+
+proxyMethods.forEach((name) => {
+  LazyList.prototype[name] = function (...args) {
+    return this.getList()[name](...args)
+  }
+})

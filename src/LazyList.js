a333db5f852b5f1cb1fa12482d01e874e1b361ed
Renée Kooi <renee@kooi.me>
2017-06-14 14:15:08 +0200

    Use prop-types

--- src/LazyList.js
+++ src/LazyList.js
@@ -1,4 +1,5 @@
-import { cloneElement, Component, PropTypes } from 'react'
+import { cloneElement, Component } from 'react'
+import PropTypes from 'prop-types'
 
 const proxyMethods = [
   'getOffset',
